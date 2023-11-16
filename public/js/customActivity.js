31873a87bb31c01c8af1f4c98c029600e14bc40a
Vinícius de Souza Troyack <troyackvinicius@gmail.com>
2023-11-16 17:51:31 -0300

    Fix: html script

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,58 +1,60 @@
-"use strict";
-const Postmonger = require('./libs/postmonger')
-var connection = new Postmonger.Session();
+define(["postmonger"], function (Postmonger) {
+  "use strict";
+
+  var connection = new Postmonger.Session();
 
 
-function carregarCheckboxesDaDataExtension() {
+  function carregarCheckboxesDaDataExtension() {
 
-  const camposDataExtension = [
-    { name: 'Campo1', label: 'Campo 1' },
-    { name: 'Campo2', label: 'Campo 2' },
-    { name: 'Campo3', label: 'Campo 3' }
-  ];
+    const camposDataExtension = [
+      { name: 'Campo1', label: 'Campo 1' },
+      { name: 'Campo2', label: 'Campo 2' },
+      { name: 'Campo3', label: 'Campo 3' }
+    ];
 
-  const checkboxesFieldset = $('#checkboxes');
+    const checkboxesFieldset = $('#checkboxes');
 
 
-  camposDataExtension.forEach((campo) => {
-    checkboxesFieldset.append(`
+    camposDataExtension.forEach((campo) => {
+      checkboxesFieldset.append(`
       <input type="checkbox" id="${campo.name}" value="${campo.name}">
       <label for="${campo.name}">${campo.label}</label><br>
     `);
-  });
-}
-
-
-$(document).ready(() => {
-  carregarCheckboxesDaDataExtension();
-});
-
-connection.on('done', function () {
-
-  const assunto = $('#assunto').val();
-  const comentario = $('#comentario').val();
-  const abriuEmail = $('#abriuEmail').val();
-  const respondeuSMS = $('#respondeuSMS').val();
-  const prioridade = $('prioridade').val()
-  // Array para armazenar os campos da Data Extension selecionados
-  const camposSelecionados = [];
-  // Percorrer todos os checkboxes e verificar se estão selecionados
-  $('[type="checkbox"]').each(function () {
-    if ($(this).is(':checked')) {
-      camposSelecionados.push($(this).val());
-    }
-  });
-
-  const payload = {
-    assunto: assunto,
-    prioridade: prioridade,
-    comentartios: {
-      comentario: comentario,
-      abriuEmail: abriuEmail,
-      respondeuSMS: respondeuSMS,
-      camposSelecionados: camposSelecionados
-    },
+    });
   }
 
-  connection.trigger('updateActivity', payload);
-});
+
+  $(document).ready(() => {
+    carregarCheckboxesDaDataExtension();
+  });
+
+  connection.on('done', function () {
+
+    const assunto = $('#assunto').val();
+    const comentario = $('#comentario').val();
+    const abriuEmail = $('#abriuEmail').val();
+    const respondeuSMS = $('#respondeuSMS').val();
+    const prioridade = $('prioridade').val()
+    // Array para armazenar os campos da Data Extension selecionados
+    const camposSelecionados = [];
+    // Percorrer todos os checkboxes e verificar se estão selecionados
+    $('[type="checkbox"]').each(function () {
+      if ($(this).is(':checked')) {
+        camposSelecionados.push($(this).val());
+      }
+    });
+
+    const payload = {
+      assunto: assunto,
+      prioridade: prioridade,
+      comentartios: {
+        comentario: comentario,
+        abriuEmail: abriuEmail,
+        respondeuSMS: respondeuSMS,
+        camposSelecionados: camposSelecionados
+      },
+    }
+
+    connection.trigger('updateActivity', payload);
+  })
+})
