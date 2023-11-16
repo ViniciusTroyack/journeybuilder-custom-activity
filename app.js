60aa427f4ae78c1d27c07cc19cfec616de276554
Vinícius de Souza Troyack <troyackvinicius@gmail.com>
2023-11-16 17:03:38 -0300

    Fix: postmonger

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
+
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const routes = require('./routes');
--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -6,20 +6,18 @@ var connection = new Postmonger.Session();
 var payload = {};
 var eventDefinitionKey = null;
 
-// Função para carregar checkboxes dinâmicos para cada campo da Data Extension
+
 function carregarCheckboxesDaDataExtension() {
-  // Simulação de dados estáticos para os campos da Data Extension
+
   const camposDataExtension = [
     { name: 'Campo1', label: 'Campo 1' },
     { name: 'Campo2', label: 'Campo 2' },
     { name: 'Campo3', label: 'Campo 3' }
-    // Simule ou obtenha dinamicamente os campos da Data Extension aqui
   ];
 
-  // Selecionar o elemento do fieldset para os checkboxes
   const checkboxesFieldset = $('#checkboxes');
 
-  // Preencher o fieldset com os checkboxes para cada campo da Data Extension
+
   camposDataExtension.forEach((campo) => {
     checkboxesFieldset.append(`
       <input type="checkbox" id="${campo.name}" value="${campo.name}">
@@ -28,14 +26,13 @@ function carregarCheckboxesDaDataExtension() {
   });
 }
 
-// Evento de carregamento da página
+
 $(document).ready(() => {
-  // Chamar a função para carregar checkboxes dos campos da Data Extension
   carregarCheckboxesDaDataExtension();
 });
 
-// Evento de clique no botão "Enviar Dados"
-connection.on('done', function () {
+$('#enviarDados').on('click', () => {
+  //connection.on('done', function () {
 
   const assunto = $('#assunto').val();
   const comentario = $('#comentario').val();
@@ -61,6 +58,6 @@ connection.on('done', function () {
       camposSelecionados: camposSelecionados
     },
   }
-
-  connection.trigger('updateActivity', payload);
+  console.log(payload)
+  //connection.trigger('updateActivity', payload);
 });
