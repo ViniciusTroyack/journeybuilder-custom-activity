fb3529084cf774764187bd1bcf3583b2b794219c
Vinícius de Souza Troyack <troyackvinicius@gmail.com>
2023-11-16 15:17:49 -0300

    Fix: Routs

--- routes.js
+++ routes.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const zendeskController = require('./zendeskController');
 
-router.post('/createTicket', zendeskController.createZendeskTicket);
+router.post('/execute', zendeskController.createZendeskTicket);
+router.post("/publish", async (req, res) => {
+    res.end();
+});
+router.post("/save", async (req, res) => {
+    res.end();
+});
+router.post("/validade", async (req, res) => {
+    res.end();
+});
+router.post("/stop", async (req, res) => {
+    res.end();
+});
+
 
 module.exports = router;
